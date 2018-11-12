2c5e959fd27ac178f624b7d4f834b805fc55bfdc
Alex Bennée <alex.bennee@linaro.org>
2018-11-12 20:10:14 +0000

    build.sh: paramterise USER/UID for the distro builds

--- build.sh
+++ build.sh
@@ -3,6 +3,10 @@
 # Build a docker image with a simple call basing the tag on the directory name
 
 NAME=$(id -un)
+USERID=$(id -u)
 TAG=$(basename ${PWD})
 
-docker build --build-arg USER=${NAME} -t ${NAME}:${TAG} .
+docker build \
+       --build-arg USER=${NAME} \
+       --build-arg UID=${USERID} \
+       -t ${NAME}:${TAG} .
