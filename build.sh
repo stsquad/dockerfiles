7a3d20f6cc1946de7942dadbc8f51ee42cc8ba33
Alex Bennée <alex.bennee@linaro.org>
2025-08-06 16:40:31 +0100

    build.sh: cleanups to script

--- build.sh
+++ build.sh
@@ -1,10 +1,15 @@
 #!/bin/sh
 #
-# Build a docker image with a simple call basing the tag on the directory name
+# Build a docker image with a simple call basing the tag on the
+# directory name. Call from the Dockerfile directory, e.g.:
+#
+#   cd distros/debian-trixie
+#   ../../build.sh
+#
 
 NAME=$(id -un)
 USERID=$(id -u)
-TAG=$(basename ${PWD})
+TAG=$(basename "$PWD")
 
 cmd_c=$(command -v "docker" || command -v "podman")
 
